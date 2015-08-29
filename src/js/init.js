56105e4d12b03e0b11fa87e12b199f1172cb143d
Alex Milanov <alex@i4web.biz>
2015-08-29 19:04:32 +0300

    8-first-conversion-test
    https://trello.com/c/dOnHo632/8-first-conversion-test

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,10 @@
 "use strict";
 
 $(document).ready(function(){
-	
+	$("#convert-markdown-html").click(function(){
+		var md = $("#markdown-editor").val();
+		console.log(md);
+		console.log(marked(md));
+		$("#html-editor").html(marked(md));
+	})
 })
