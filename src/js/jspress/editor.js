a1464ce15ec6d073366f485fd87e4cc02b800818
Alex Milanov <alex@i4web.biz>
2015-10-08 17:37:10 +0300

    22-editor-refactor-selection-algorithm
    https://trello.com/c/9vpFXDuz/22-editor-refactor-selection-algorithm
    
    - rework get/set selection
    - changemap generation
    - adequately position pointer after text/p -> h1/h2
    - preserve newline nodes in wysiwyg editor

--- src/js/jspress/editor.js
+++ src/js/jspress/editor.js
@@ -26,11 +26,160 @@ jspress.Editor = function(dom, options){
 			}
 		})
 	}
+
+
+	this._getNodePath = function(parent, child){
+		if(!parent.contains(child))
+			return false;
+		var node = child;
+		var nodePath = [];
+
+		nodePath.push(Array.prototype.indexOf.call(node.parentNode.childNodes, node));
+
+		while(node.parentNode != parent) {
+			node = node.parentNode;
+			nodePath.push(Array.prototype.indexOf.call(node.parentNode.childNodes, node));
+		}
+
+		nodePath.reverse();
+		return nodePath;
+	}
+
+	this._nodeByPath = function(parent, nodePath){
+		var node = parent;
+
+		nodePath.forEach(function(index){
+			if(node.childNodes[index]){
+				if(node.childNodes[index].nodeType == 3 && node.childNodes[index].textContent == "\n"){
+					if(node.childNodes[index+1])
+						node = node.childNodes[index+1];
+					else 
+						return false;
+				} else {
+					node = node.childNodes[index];
+				}
+			}
+			else 
+				return false;
+		})
+
+		// select a text node if it's present
+		if(node.childNodes[0] && node.childNodes[0].nodeType == 3){
+			node = node.childNodes[0];
+		}
+
+		return node;
+	}
+
+	this._getSelection = function(){
+
+		var containerEl = this._wysiwyg[0];
+
+		var sel = window.getSelection();
+		//console.log(containerEl);
+		var range = window.getSelection().getRangeAt(0);
+		var preSelectionRange = range.cloneRange();
+		preSelectionRange.selectNodeContents(containerEl);
+		preSelectionRange.setEnd(range.startContainer, range.startOffset);
+		var start = preSelectionRange.toString().length;
+
+		return {
+			start: start,
+			startNodePath: this._getNodePath(containerEl, sel.anchorNode),
+			startNodeOffset: sel.anchorOffset,
+			end: start + range.toString().length,
+			endNodePath: this._getNodePath(containerEl, sel.extentNode),
+			endNodeOffset: sel.extentOffset
+		};
+	}
+
+	this._setSelection = function(changeMap, savedSel){
+
+		var containerEl = this._wysiwyg[0];
+		
+		var charIndex = 0, range = document.createRange();
+		range.setStart(containerEl, 0);
+		range.collapse(true);
+
+		for(var index in changeMap){
+			var change = changeMap[index];
+			switch(change.kind){
+				case "D":
+					console.log(change, index);
+					var newNode = document.createElement(change.lhs.name);
+					newNode.appendChild(document.createElement("br"));
+					if(containerEl.childNodes[index]){
+						containerEl.insertBefore(newNode, containerEl.childNodes[index]);
+					} else {
+						containerEl.appendChild(newNode);
+					}
+					break;
+				case "E":
+					if(savedSel.startNodePath[0] == index 
+						|| ( savedSel.startNodePath[0] == index+1 && containerEl.childNodes[index].nodeType == 3 )){
+						if(change.changes.name){
+							if(["H1","H2","H3","H4","LI"].indexOf(change.changes.name.rhs) > -1){
+								console.log("changed",change.changes.content.rhs.length)
+								savedSel.startNodeOffset = change.changes.content.rhs.length;
+							}
+						}
+					}
+					if(savedSel.endNodePath[0] == index
+						|| ( savedSel.endNodePath[0] == index+1 && containerEl.childNodes[index].nodeType == 3 )){
+						if(change.changes.name){
+							if(["H1","H2","H3","H4","LI"].indexOf(change.changes.name.rhs) > -1){
+								savedSel.endNodeOffset = change.changes.content.rhs.length;
+							}
+						}
+					}
+					break;
+			}
+		}
+		console.log(changeMap, savedSel);
+		
+		range.setStart(this._nodeByPath(containerEl,savedSel.startNodePath),savedSel.startNodeOffset);
+		range.setEnd(this._nodeByPath(containerEl,savedSel.endNodePath),savedSel.endNodeOffset);
+		
+
+		var sel = window.getSelection();
+		sel.removeAllRanges();
+		sel.addRange(range);
+	}
+}
+
+jspress.Editor.prototype.processDiffMap = function(diffMap, selection){
+	if(!diffMap)
+		return false;
+	var editor = this;
+	var changeMap = {};
+	diffMap.forEach(function(diffNode){
+		var kind = diffNode.kind;
+		var index, item;
+		switch(kind){
+			case "E":
+				index = diffNode.path[0];
+				break;
+			case "A":
+				index = diffNode.index;
+				diffNode = diffNode.item;
+				kind = diffNode.kind;
+				break;
+		}
+		if(!changeMap[index])
+			changeMap[index] = { changes: [], kind: "E" };
+		if(diffNode.path && diffNode.path.length > 0){
+			changeMap[index].changes[diffNode.path[diffNode.path.length-1]] = _.cloneDeep(diffNode);
+		} else {
+			changeMap[index] = diffNode;
+		}
+
+	})
+	return changeMap;
 }
 
 
 jspress.Editor.prototype.process = function(){
-	var sel = saveSelection(this._wysiwyg[0]);
+	var sel = this._getSelection(this._wysiwyg[0]);
 	
 	this._generateNodeMap();
 	var oldMap = _.clone(this._nodeMap);
@@ -46,9 +195,10 @@ jspress.Editor.prototype.process = function(){
 	var newMap = _.clone(this._nodeMap);
 
 	var diffMap = DeepDiff.diff(oldMap,newMap);
-	console.log(diffMap,oldMap,newMap);
+	//console.log(diffMap,oldMap,newMap);
+	var changeMap = this.processDiffMap(diffMap);
 
-	restoreSelection(this._wysiwyg[0],sel);
+	this._setSelection(changeMap,sel);
 }
 
 
